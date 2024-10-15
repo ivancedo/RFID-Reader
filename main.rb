ae30d9b9cb639c3358387ab8a149604a6699454e
ivancedo <ivancedo09@gmail.com>
2024-10-15 19:55:41 +0200

    hex_uid: Changes made in order to reverse the bytes from the given UID.

--- main.rb
+++ main.rb
@@ -10,14 +10,18 @@ class RFID
         self.uid = uid
     end
 
-    # Converts the UID to a hexadecimal string.
+    # Converts the given UID to a hexadecimal string with its bytes reversed.
     #
     # @return [String, nil] The UID as a hexadecimal string in uppercase, or `nil`
     #   if the conversion to integer fails.
     # @raise [ArgumentError] If the UID cannot be converted to an integer.
     def hex_uid
         begin
-            Integer(uid).to_s(16).upcase
+            decimal_uid = uid.to_i
+            bytes_array = [decimal_uid].pack('L').bytes.reverse
+            decimal_array = bytes_array.pack('C*').unpack('L')
+            hex_string = decimal_array[0].to_s(16).upcase
+            return hex_string
         rescue ArgumentError
             nil
         end
